5ed8e7aad0cf7241092a46333d8326e7a4565c94
yebin <rebin0313@gmail.com>
2022-07-01 16:28:23 +0900

    AOP

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+    //AOP 적용하면 가짜 memberService 프록시 참조 이후, joinPoint.proceed()를 통해서 진짜 memberService 연결
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try{
+            return joinPoint.proceed(); // 다음 프로세스로 넘어간다는 뜻
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() +" " + timeMs + "ms");
+        }
+    }
+}
